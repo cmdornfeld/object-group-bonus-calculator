dd3c0b36e94165a76d7ef84516f30ca885725279
Chad Dornfeld <cmdornfeld@gmail.com>
2019-11-05 17:03:37 -0600

    created bonus and newEmployee functions

--- client.js
+++ client.js
@@ -41,3 +41,50 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+for (let i = 0; i < employees.length; i++) {
+  console.log(employees[i]);
+}
+
+function createNewEmployee (employee) {
+  calculateBonus(employee);
+  let bonusPercentage = calculateBonus(employee);
+  let totalBonus = bonusPercentage * employee.annualSalary;
+  let totalCompensation = totalBonus + Number(employee.annualSalary);
+  let newEmployee = {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: totalCompensation,
+    totalBonus: totalBonus
+  }
+  return newEmployee;
+}
+// end createNewEmployee
+
+function calculateBonus(employee) {
+  if (employees.reviewRating <= 2) {
+    let bonusPercentage = 0;
+    return bonusPercentage;
+  }
+  else if (employees.reviewRating === 3) {
+    bonusPercentage = .04;
+    return bonusPercentage;
+  }
+  else if (employee.reviewRating === 4){
+    bonusPercentage = .06;
+    return bonusPercentage;
+  }
+  else if (employee.reviewRating === 5){
+    bonusPercentage = .1;
+    return bonusPercentage;
+  }
+}
+
+/* function yearsWithCompany(employee) {
+  if (employee.employeeNumber.length === 4 ) {
+    let extraBonusPercentage = 5;
+  }
+  else
+  return
+}*/
+
